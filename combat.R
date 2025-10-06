0582458d3f2280460345e3fc2feaaa960fa7c156
samrat-lab <118902303+samrat-lab@users.noreply.github.com>
2025-10-06 16:27:11 +0530

    Update combat.R

--- combat.R
+++ combat.R
@@ -1,7 +1,10 @@
 library(sva)
 
+############# dat will be your input expression file ##################################
 dat = read.csv("Combat_matrix_input.csv");
+######## sif will be your meta file which stores info about which samples belongs to which dataset and their condition ################################
 sif = read.csv("sif.csv");
+
 modcombat = model.matrix(~1, data=dat)
 
 newdata = ComBat(dat=dat, batch=sif$Batch, mod=NULL, par.prior=TRUE, prior.plots=TRUE)
